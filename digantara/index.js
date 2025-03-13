4d3aa539222d8617cd66a4109fe4c78f4a6a1f6f
musharruf <mdmusharruf13@gmail.com>
2025-03-14 01:12:57 +0530

    displaying and hiding inputs on navigation

--- digantara/index.js
+++ digantara/index.js
@@ -1,3 +1,5 @@
+import { disableBtn, enableBtn, showPage } from "./utils/helper.js";
+
 export const formObj = {
     userName: '',
     dateOfBirth: '',
@@ -6,6 +8,10 @@ export const formObj = {
     phone: '',
     address: ''
 }
+
+let pages = [];
+let currentPage = 0;
+
 
 function handleInputChange(event) {
     const name = event.target.name;
@@ -18,9 +24,48 @@ function handleAttachEventListener(element) {
     element.addEventListener("input", handleInputChange);
 }
 
+let showCurrentPage = null;
+
+const nextBtn = document.getElementById("next");
+const backBtn = document.getElementById("back");
+
+
+nextBtn.addEventListener("click", () => {
+    if (currentPage + 1 == pages.length - 1) {
+        nextBtn.value = "Submit";
+    }
+    if (currentPage + 1 < pages.length) {
+        showCurrentPage(++currentPage);
+        enableBtn(backBtn);
+    }
+});
+
+backBtn.addEventListener("click", () => {
+    nextBtn.value = "Next";
+    if (currentPage - 1 > 0) {
+        showCurrentPage(--currentPage);
+    } else if (currentPage - 1 === 0) {
+        showCurrentPage(--currentPage);
+        disableBtn(backBtn);
+    }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const inputField = document.getElementsByClassName("user-input");
     for (let i = 0; i < inputField.length; i++) {
         handleAttachEventListener(inputField[i]);
     }
+
+    const pagesSection = document.querySelectorAll(".pages");
+    pagesSection.forEach((ele, index) => pages.push(index));
+
+    const formTwo = document.getElementById("form-two");
+    const summary = document.getElementById("summary");
+
+    formTwo.style.display = "none";
+    summary.style.display = "none";
+
+    showCurrentPage = showPage("pages");
+    showCurrentPage(currentPage);
+    disableBtn(backBtn);
 });
